0bfc587fa3596ee95d2e1b2e490f8f5bcba6b6b5
mozoezomoe <dres2112@gmail.com>
2023-01-24 12:55:33 +0100

    new block

--- scripts/10.testingNormalization.R
+++ scripts/10.testingNormalization.R
@@ -10,7 +10,9 @@ library(clValid)
 library(mclust)
 library(factoextra)
 library(ggpubr)
-
+library(randomForest)
+library(e1071)
+library(h2o)
 
 # scripts
 
@@ -27,11 +29,52 @@ table1.1 <- anthroSex(table1.0)
 table1.2 <- normalizingNumeric(table1.1)
 table1.3 <- timingCleanFeatures(table1.2, "data/chronicPlasmAnt.csv")
 
+# test for random filling ----
+
+randomFilling <- function(tabla){
+  
+  for (var in names(tabla)){
+  cat(var,"\n") # Print the variable name for debug purposes
+  flush.console()
+  
+  if (class(tabla[[var]]) == "numeric"){
+    
+    # random number into the var variance
+  
+    #for (i in seq(1, length(tabla[is.na(tabla[[var]]), var]))){
+    
+      
+    tabla[is.na(tabla[[var]]), var] <- runif(length(tabla[is.na(tabla[[var]]), var]),
+                                             min= min(na.omit(tabla[[var]])), 
+                                             max= max(na.omit(tabla[[var]])))
+    
+    
+    #}
+    
+    }
+  
+  
+  } 
+  
+  return(tabla)
+}
+
+
+table1.4_random <- randomFilling(table1.3)
+
+table1.3[is.na(table1.3[["CA"]]), "CA"]
+
 # pre-imputating test
 
-library(randomForest)
 
 table1.3$Sex <- factor(table1.3$Sex)
+table1.3$Sweetener <- factor(table1.3$Sweetener)
+table1.3$Time <- factor(table1.3$Time)
+table1.3 <- table1.3 %>% select (-c(numVol, grouping, Weight, BMI, Fat, CVRI, Bpmin, Bpmax, Frec))
+
+
+# rf
+
 
 rf_noimp <- randomForest(Sex ~., data = table1.3, na.action = na.omit)
   
@@ -39,6 +82,57 @@ table1.4 <- factoringImputating(table1.3)
 
 rf_imp <- randomForest(Sex ~., data = table1.4)
 
+rf_impRan <- randomForest(Sex ~ ., data = table1.4_random)
+
+# svm
+
+svm_tuning_noimp <- tune("svm", Sex ~ ., data = table1.3, kernel = "linear",
+                  ranges = list(cost = c(0.0001, 0.0005, 0.001, 0.01, 0.1, 1)),
+                  scale = TRUE)
+
+ggplot(data = svm_noimp$performances, aes(x = cost, y = error)) +
+  geom_line() +
+  geom_point() +
+  labs(title = "Error de clasificación vs hiperparámetro C") +
+  theme_bw()
+
+svm_tuning_noimp$best.parameters
+
+modelo_svc <- svm(Sex ~ ., data = table1.3, 
+                  kernel = "linear", 
+                  cost = 1, 
+                  scale = TRUE)
+
+predicciones = predict(modelo_svc, table1.3)
+
+sex_uwu <- na.omit(table1.3)
+sex_NoNa <- sex_uwu$Sex
+
+confusionMatrix(predicciones, sex_NoNa)
+
+table1.4 <- factoringImputating(table1.3)
+
+svm_tuning_imp <- tune("svm", Sex ~ ., data = table1.4, kernel = "linear",
+                       ranges= list(cost = c(0.0001, 0.0005, 0.001, 0.01, 0.1, 1)),
+                       scale = TRUE)
+svm_tuning_imp$best.parameters
+
+modelo_svc_imp <- svm(Sex ~ ., data = table1.4, 
+                      kernel = "linear", 
+                      cost = 10, 
+                      scale = TRUE)
+
+predicciones_imp = predict(modelo_svc_imp, table1.4)
+
+confusionMatrix(predicciones_imp, table1.4$Sex)
+
+
+
+# ann
+
+
+
+# main test function
 
 mainTest <- function(pathToTable){
   
@@ -54,11 +148,12 @@ mainTest <- function(pathToTable){
   
   rf_noimp <- randomForest(Sex ~., data = table1.3
                            , na.action = na.omit)
+  svm_noimp <- 
   
   table1.4 <- factoringImputating(table1.3)
   
   rf_imp <- randomForest(Sex ~., data = table1.4, na.action = na.omit)
-  
+  svm
   
   return(list(rf_noimp, rf_imp))
 }
